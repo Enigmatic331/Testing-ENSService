a52b8d1dd036d130b2b4b5a1f8ac3875dde17ee3
Enigmatic331 <unreal_legend7@hotmail.com>
2020-04-20 12:51:15 +0800

    Add files

---
+++ EthereumUtil/Forms/MainForm.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnExec = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnExec
+        '
+        Me.btnExec.Location = New System.Drawing.Point(7, 69)
+        Me.btnExec.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExec.Name = "btnExec"
+        Me.btnExec.Size = New System.Drawing.Size(335, 32)
+        Me.btnExec.TabIndex = 0
+        Me.btnExec.Text = "Click to SEE IT ALL"
+        Me.btnExec.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(119, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(222, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter ENS Address:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(7, 117)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(334, 76)
+        Me.TextBox2.TabIndex = 3
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(353, 205)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnExec)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExec As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ EthereumUtil/Forms/MainForm.vb
@@ -0,0 +1,38 @@
+﻿Imports Nethereum.Web3
+Imports Nethereum.ENS
+
+
+Public Class MainForm
+    Private Async Sub testContract()
+
+        Dim iweb3 = New Web3("https://cloudflare-eth.com")
+        Dim ensService = New ENSService(iweb3)
+
+        Dim content = Await ensService.GetContentHashAsync(TextBox1.Text.Trim()) 'need to
+        If content Is Nothing Then
+            TextBox2.Text = "Nada"
+        Else
+            If content.Length = 0 Then
+                TextBox2.Text = "Doesn't seem to have a contentHash/ipfs address"
+            Else
+                If content(0) = &HE3 Then
+                    Dim cid = IPLD.ContentIdentifier.Cid.Cast(content.Skip(2).ToArray())
+                    Dim decoded = cid.Hash.B58String() 'lol naw this ain't obsolete yo, don't wanna convert to base58 myself
+                    TextBox2.Text = decoded
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Public Function RemovePrefix(ByVal input As String) As String
+        If input.Substring(0, 2) = "0x" Then
+            input = input.Substring(2)
+        End If
+        Return input
+    End Function
+
+    Private Sub btnExec_Click(sender As Object, e As EventArgs) Handles btnExec.Click
+        Call testContract()
+    End Sub
+End Class
